1f4fdd368c3bf3312400853c85bb9ff9bd519624
Patrick Franzelin <181646124+patrickfranzelin@users.noreply.github.com>
2024-10-10 01:11:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ZjQ2MzU4Zi1kMWQ0LTQ0MTUtODU0OS0zZTI1NjhiN2FmZDMiLCJpZCI6MjQ2OTE4LCJpYXQiOjE3Mjg1MDk4NzN9.DbWm6ADaeMLM-K8qIISh9vi6QlU281OA30AQ6_mDs70';
+
 // Initialize the Cesium Viewer with no initial imagery provider
 const viewer = new Cesium.Viewer("cesiumContainer", {
   imageryProvider: false, // Start with no imagery provider
