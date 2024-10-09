122771cfa0e31602ffa318b22a721a99db23f73b
Patrick Franzelin <181646124+patrickfranzelin@users.noreply.github.com>
2024-10-10 00:42:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Set your Cesium Ion Access Token
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ZjQ2MzU4Zi1kMWQ0LTQ0MTUtODU0OS0zZTI1NjhiN2FmZDMiLCJpZCI6MjQ2OTE4LCJpYXQiOjE3Mjg1MDk4NzN9.DbWm6ADaeMLM-K8qIISh9vi6QlU281OA30AQ6_mDs70';
+
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
 const viewer = new Cesium.Viewer("cesiumContainer", {
   imageryProvider: new Cesium.IonImageryProvider({ assetId: 3954 }), // Satellite imagery asset ID
