99d8a1a29c2ba10ca51ddbc0c246063f46001b45
Patrick Franzelin <181646124+patrickfranzelin@users.noreply.github.com>
2024-10-11 20:27:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Grant CesiumJS access to your ion assets
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ZjQ2MzU4Zi1kMWQ0LTQ0MTUtODU0OS0zZTI1NjhiN2FmZDMiLCJpZCI6MjQ2OTE4LCJpYXQiOjE3Mjg1MDk4NzN9.DbWm6ADaeMLM-K8qIISh9vi6QlU281OA30AQ6_mDs70";
 
 async function initializeViewer() {
   try {
-    // Initialize the Cesium Viewer with custom options
+    // Initialize the Cesium Viewer without a terrainProvider for initial setup
     const viewer = new Cesium.Viewer("cesiumContainer", {
-      baseLayerPicker: false,       // Show base layer picker
-      imageryProvider: false,      // Disable default imagery provider initially
+      baseLayerPicker: true,
+      imageryProvider: true,  // Disable default imagery
     });
 
     // Load and set the custom terrain provider
@@ -14,7 +13,7 @@ async function initializeViewer() {
     viewer.terrainProvider = terrainProvider;
 
     // Load the custom imagery layer
-    const imageryProvider = await Cesium.IonImageryProvider.fromAssetId(2764800);
+    const imageryProvider = await Cesium.IonImageryProvider.fromAssetId(2764816);
     viewer.imageryLayers.addImageryProvider(imageryProvider);
 
     // Set the initial view to a specific area
@@ -26,7 +25,7 @@ async function initializeViewer() {
       },
     });
   } catch (error) {
-    console.error("An error occurred while initializing the Cesium viewer:", error);
+    console.error("An error occurred while initializing Cesium viewer:", error);
   }
 }
 
