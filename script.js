cd715d35f87a0c9b6f08ce0361cc8db1a4664992
Patrick Franzelin <181646124+patrickfranzelin@users.noreply.github.com>
2024-10-11 12:37:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 // Grant CesiumJS access to your ion assets
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ZjQ2MzU4Zi1kMWQ0LTQ0MTUtODU0OS0zZTI1NjhiN2FmZDMiLCJpZCI6MjQ2OTE4LCJpYXQiOjE3Mjg1MDk4NzN9.DbWm6ADaeMLM-K8qIISh9vi6QlU281OA30AQ6_mDs70";
 
-async function initializeViewer() {
-  // Initialize the Cesium Viewer with Cesium World Terrain (3D)
+try {
   const viewer = new Cesium.Viewer("cesiumContainer", {
-    terrainProvider: Cesium.createWorldTerrain(),
-    baseLayerPicker: false,
+    terrainProvider:
+      await Cesium.CesiumTerrainProvider.fromIonAssetId(1),
   });
-
+} catch (error) {
+  console.log(error);
 }
-
-// Call the function to initialize the Cesium viewer
-initializeViewer();
