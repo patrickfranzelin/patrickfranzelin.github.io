1b54887834e1b19b51219e5fae370079c9c83ec5
Patrick Franzelin <181646124+patrickfranzelin@users.noreply.github.com>
2024-10-11 20:31:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+// Grant CesiumJS access to your ion assets
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0ZjQ2MzU4Zi1kMWQ0LTQ0MTUtODU0OS0zZTI1NjhiN2FmZDMiLCJpZCI6MjQ2OTE4LCJpYXQiOjE3Mjg1MDk4NzN9.DbWm6ADaeMLM-K8qIISh9vi6QlU281OA30AQ6_mDs70";
 
 async function initializeViewer() {
   try {
-    // Initialize the Cesium Viewer without a terrainProvider for initial setup
+    // Initialize the Cesium Viewer with custom options
     const viewer = new Cesium.Viewer("cesiumContainer", {
       baseLayerPicker: true,
-      imageryProvider: true,  // Disable default imagery
+      imageryProvider: false  // Disable default imagery provider
     });
 
     // Load and set the custom terrain provider
@@ -21,8 +22,8 @@ async function initializeViewer() {
       destination: Cesium.Cartesian3.fromDegrees(11.362, 46.498, 15000),  // Adjust coordinates and altitude as needed
       orientation: {
         heading: Cesium.Math.toRadians(0),
-        pitch: Cesium.Math.toRadians(-30),
-      },
+        pitch: Cesium.Math.toRadians(-30)
+      }
     });
   } catch (error) {
     console.error("An error occurred while initializing Cesium viewer:", error);
